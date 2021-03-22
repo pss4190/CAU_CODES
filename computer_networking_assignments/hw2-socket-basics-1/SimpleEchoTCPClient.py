44ad7c7ad85317e38dca8ff0ac458746f72e5fb4
Franklin Park <pss4190@naver.com>
2021-03-22 17:15:49 +0900

    - until option_3 implemented - error handling implemented ( ctrl c exit and total handling )

--- computer_networking_assignments/hw2-socket-basics-1/SimpleEchoTCPClient.py
+++ computer_networking_assignments/hw2-socket-basics-1/SimpleEchoTCPClient.py
@@ -3,12 +3,71 @@
 #
 
 from socket import *
-from TCPClientFunctions import *
+import time
 
 # 127.0.0.1 for localhost ( need to modify into nsl2.cau.ac.kr )
 serverName = '127.0.0.1'
 serverPort = 24435
 
+# function for option 1 : upper-case
+def option_1() :
+    # take sending message from user
+    sending_message = input('Input sentense: ')
+
+    # send user input to server
+    # send with option number so can run proper implement on server as wished
+    # also get current time to calculate duration of sending/receiving moment
+    sending_message = '1' + sending_message
+    sending_time = time.time()
+    clientSocket.send(sending_message.encode())
+
+    # receive result
+    # also get current time to calculate duration of sending/receiving moment
+    received_message = clientSocket.recv(2048)
+    received_time = time.time()
+
+    # print out results
+    print('\nReply from server:', received_message.decode())
+    print('Response latency =', str((received_time-sending_time) * 1000), 'ms\n')
+
+# function for option 2 : reverse-order
+def option_2() :
+    # take sending message from user
+    sending_message = input('Input sentense: ')
+
+    # send user input to server
+    # send with option number so can run proper implement on server
+    # also get current time to calculate duration of sending/receiving moment
+    sending_message = '2' + sending_message
+    sending_time = time.time()
+    clientSocket.send(sending_message.encode())
+
+    # receive result
+    # also get current time to calculate duration of sending/receiving moment
+    received_message = clientSocket.recv(2048)
+    received_time = time.time()
+
+    # print out results
+    print('\nReply from server:', received_message.decode())
+    print('Response latency =', str((received_time-sending_time) * 1000), 'ms\n')
+
+# function for option 3 : client IP & port
+def option_3() :
+    # only sends user option value
+    # also get current time to calculate duration of sending/receiving moment
+    sending_time = time.time()
+    clientSocket.send("3".encode())
+
+    # receive result
+    # also get current time to calculate duration of sending/receiving moment
+    received_message = clientSocket.recv(2048)
+    received_time = time.time()
+
+    # print out results
+    print('\nReply from server:', received_message.decode())
+    print('Response latency =', str((received_time-sending_time) * 1000), 'ms\n')
+
+# initial establish connection to server
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
@@ -23,27 +82,32 @@ offering_functionality = {
     5 : "5) exit"
 }
 
-# start Client Application
-while True :
-    # print out all options that client can choose
-    print("<Menu>")
-    for key in offering_functionality.keys() :
-        print(offering_functionality[key])
+try :
+    # start Client Application
+    while True :
+        # print out all options that client can choose
+        print("<Menu>")
+        for key in offering_functionality.keys() :
+            print(offering_functionality[key])
 
-    # wait for user's selection input
-    print("Input option: ")
-    client_option_value = int(input())
+        # wait for user's selection input
+        print("Input option: ", end=' ')
+        client_option_value = int(input())
 
-    if(client_option_value == 1) :
-        print("option_1 start")
-        option_1(clientSocket)
-
-message = input('Input lowercase sentence: ')
-
-# send input message to server
-clientSocket.send(message.encode())
-
-modifiedMessage = clientSocket.recv(2048)
-print('Reply from server:', modifiedMessage.decode())
-
-clientSocket.close()
+        if(client_option_value == 1) :
+            option_1()
+        elif(client_option_value == 2) :
+            option_2()
+        elif(client_option_value == 3) :
+            option_3()
+        else :
+            print("inserted wrong option value. \ninsert again\n")
+            continue
+except KeyboardInterrupt :
+    # close connection
+    clientSocket.close()
+    print("\nBye Bye~")
+except :
+    # handles all types of error
+    print("\n unexpected error occured on client. \nTerminate client side application.")
+    clientSocket.close()
--- computer_networking_assignments/hw2-socket-basics-1/SimpleEchoTCPServer.py
+++ computer_networking_assignments/hw2-socket-basics-1/SimpleEchoTCPServer.py
@@ -1,22 +1,95 @@
 #
 # SimpleEchoTCPServer.py
 #
-
 from socket import *
+from datetime import datetime
 
+# function declaration
+def option_1(received_message, option_value) :
+    # print out requested client address
+    print('Connection requested from', clientAddress)
+    print('command', option_value)
+
+    # do action on received message
+    modified_message = received_message.upper()
+    # send modified message to client
+    connectionSocket.send(modified_message.encode())
+
+def option_2(received_message, option_value) :
+    # print out requested client address
+    print('Connection requested from', clientAddress)
+    print('command', option_value)
+
+    # do action on received message
+    msg_length = len(received_message)
+    modified_message = ''
+    # iterate message from end to start so that can reverse by one single 'for' iteration
+    for index in range(msg_length-1, -1, -1) :
+        modified_message += received_message[index]
+    
+    # send modified message to client
+    connectionSocket.send(modified_message.encode())
+
+def option_3(option_value) :
+    # print out requested client address
+    print('Connection requested from', clientAddress)
+    print('command', option_value)
+
+    # get client IP address & port number from clientAddress
+    client_ip = clientAddress[0]
+    client_port = clientAddress[1]
+    print(client_ip, " and ", client_port)
+
+    modified_message = "client IP = " + str(client_ip) + ", port = " + str(client_port)
+
+    # send message to client
+    connectionSocket.send(modified_message.encode())
+
+def option_4(option_value) :
+    # print out requested client address
+    print('Connection requested from', clientAddress)
+    print('command', option_value)
+
+    # get current time value so that can get run time
+    run_time = time.time() - initializing_time
+
+# time value to measure run time
+initializing_time = datetime.now()
+# server initializing acts
 serverPort = 24435
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 
 print("The server is ready to receive on port", serverPort)
+# accept connection requeset from client
+(connectionSocket, clientAddress) = serverSocket.accept()
 
-while True:
-    (connectionSocket, clientAddress) = serverSocket.accept()
-    print('Connection requested from', clientAddress)
-    message = connectionSocket.recv(2048)
-    print(message)
-    modifiedMessage = message.decode().upper()
-    connectionSocket.send(modifiedMessage.encode())
+try :
+    while True :
+        # get message from client
+        socket_send_message = connectionSocket.recv(2048)
+
+        # divide message into two pieces : user selection option, and user message
+        user_option = int(socket_send_message.decode()[0:1])
+        user_message = socket_send_message.decode()[1:]
+        print("user option : ", user_option)
+        print("user message : ", user_message)
+
+        if(user_option == 1) :
+            option_1(user_message, user_option)
+        elif(user_option == 2) :
+            option_2(user_message, user_option)
+        elif(user_option == 3) :
+            option_3(user_option)
+        else :
+            print("client send wrong option. \n activation denied")
+            continue
+except KeyboardInterrupt :
+    # close the connectioni
     connectionSocket.close()
-
+    print("\nBye Bye~")
+except :
+    # handles all types of error
+    print("\n Unexpected error occured on server. \nTerminate server side application.")
+    connectionSocket.close()
